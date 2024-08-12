f18d545c5782c421678f65a3d17bad1aa376eb1f
Sahil <issahil404@gmail.com>
2024-08-12 18:23:31 +0530

    added privacy policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PrivacyPolicy from "@/components/PrivacyPolicy";
+
+const PrivacyPolicyPage: React.FC = () => {
+  return <PrivacyPolicy />;
+};
+
+export default PrivacyPolicyPage;
